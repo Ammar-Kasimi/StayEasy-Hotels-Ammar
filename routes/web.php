49cd6a6d97fe993bc15b6091858327a97b1bad17
Younes Raji <rajix2100@gmail.com>
2026-02-03 16:23:13 -0500

    migrate of room, photo,tags, categories and  popreties is donne that tables working

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [AuthController::class,'index']);
+Route::get('/', function () {
+    return view('welcome', ['name' => 'James']);
+});
